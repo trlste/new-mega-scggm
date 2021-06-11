42bd3ff0cfa15a010ce396e9d8c7f7018a0f0312
trsren <yumengren_999@outlook.com>
2021-06-11 21:41:57 +0800

    newest code

--- Mega-sCGGM/q_mega_scggm.py
+++ Mega-sCGGM/q_mega_scggm.py
@@ -5,7 +5,7 @@ import numpy as np
 import numpy.random as rnd
 import scipy.sparse as ssp
 import sys
-
+import time
 sys.path.append("../Python/")
 from txt_to_sparse import txt_to_sparse
 from txt_to_dict import txt_to_dict
@@ -59,7 +59,7 @@ def q_mega_scggm(
     thisdir = os.path.dirname(os.path.abspath(__file__))
     os.chdir(thisdir) # move to demo directory
     os.chdir("../Mega-sCGGM")
-
+    t0=time.time()
     dummy = rnd.randint(low=0, high=1e6)
     V0_str = ""
     F0_str = ""
@@ -82,10 +82,7 @@ def q_mega_scggm(
         sparse_to_txt(P0file, Psi0)
         P0_str = "-T \"%s\" " % (P0file)
 
-    #n_y=n, q_prime=2q
-    #n_x=n, p_prime=2p+r
     (n_y, q_p) = Y.shape
-    #print(Y.shape)
     (n_x, p_p) = X.shape
     
     Y=Y.reshape((2*n_y,-1))
@@ -93,11 +90,6 @@ def q_mega_scggm(
     q=q_p//2
     p=p_p//2
 
-    #2n*q
-        #n*2p->2n*p
-    #n*r
-    #n*p+r
-    #n*p
     X_p=X[::2,:]
     X_m=X[1::2,:]
     X_sum=X_p+X_m
@@ -109,13 +101,6 @@ def q_mega_scggm(
     stats_sum_file = "stats_sum-dummy-%i.txt" % (dummy)
     np.savetxt(Y_sum_file, Y_sum, fmt="%.10f", delimiter=" ")
     np.savetxt(X_sum_file, X_sum, fmt="%.10f", delimiter=" ")
-    # calculates ys and xs
-    
-    #means 2n*mq
-    #print(Y_sum.shape)
-    #X_means=np.repeat(.5*X_sum, repeats=2, axis=0)
-    #print(Y_means.shape)
-    #X_complete= np.where(np.isnan(X),X_means,X)
     
 
 
@@ -125,8 +110,10 @@ def q_mega_scggm(
     X_diff=np.subtract(X_p,X_m)
     Y_diff=np.subtract(Y_p,Y_m)
     
-
+    t1=time.time()
+    print("preprocessing time is: "+str((t1-t0)//60))
     #First call to mega-scggm: V and F, the summation term
+    
     mega_str = "-l %i -t %i -m %i -n %i " % (
         num_blocks_V, num_blocks_F, memory_usage, threads)
     option_str = "-y %f -x %f -v %i -i %i -s %f -q %f -r %i  %s  %s %s " % (
@@ -138,25 +125,31 @@ def q_mega_scggm(
         n_y, q, n_x, p, Y_sum_file, X_sum_file,
         Vfile, Ffile, stats_sum_file)
     print(command_str_sum)
-    ret_sum = os.system(command_str_sum)
+    ret_sum = os.system(command_str_sum) 
+    
+    t2=time.time()
+    print("sum time is: "+str((t2-t1)//60))
+    
     V = txt_to_sparse(Vfile)
     F = txt_to_sparse(Ffile)
     stats_sum = txt_to_dict(stats_sum_file)
     
     Gamma=ssp.identity(q,format="lil")
     Psi=ssp.lil_matrix((p,q))
-
+    
+    t3=time.time()
+    print("postprocessing time is: "+str((t3-t2)//60))
+    
     for i in range(q):
+        dummy = rnd.randint(low=0, high=1e6)
         Y_diff_file= "Y_diff-dummy-%i.txt" % (dummy)
         X_diff_file= "X_diff-dummy-%i.txt" % (dummy)
         
-         
         curr_Y=Y_diff[:,i][np.logical_not(np.isnan(Y_diff[:,i]))].reshape(-1,1)
         (n_curr_y,_)=curr_Y.shape
-
+        print(n_curr_y)
         if(n_curr_y==0):
             continue
-
 
         np.savetxt(Y_diff_file, curr_Y, fmt="%.10f", delimiter=" ")
         np.savetxt(X_diff_file, X_diff, fmt="%.10f", delimiter=" ")
@@ -175,7 +168,7 @@ def q_mega_scggm(
             option_str,
             n_curr_y, 1, n_x, p, Y_diff_file, X_diff_file,
             Gammafile, Psifile, stats_diff_file)
-        print(command_str_diff)
+    #    print(command_str_diff)
         ret_diff = os.system(command_str_diff)
         #1*1
         try:
@@ -196,12 +189,13 @@ def q_mega_scggm(
 
 
     
+    t4=time.time()
+    print("diff time is: "+str((t4-t3)//60))
     
-    #stats_diff = txt_to_dict(stats_diff_file)
     Gamma=Gamma.tocoo()
     Psi=Psi.tocoo()
 
     rmline = "rm %s %s %s %s %s" % (Y_sum_file, X_sum_file, Vfile, Ffile,stats_sum_file)
     ret = os.system(rmline)
     os.chdir(olddir)
-    return (V, F, Gamma, Psi, stats_sum)#, stats_diff)
+    return (V,F,Gamma, Psi,stats_sum)
--- Mega-sCGGM/test_q.py
+++ Mega-sCGGM/test_q.py
@@ -9,16 +9,16 @@ import sys
 from mega_scggm import mega_scggm
 from q_mega_scggm import q_mega_scggm
 
-n = 500
+n = 1000
 m = 2
 p = 50
 q = 50
-nan_percent_col=0.05
+nan_percent_col=0.2
 #r = 5
 
 X=np.random.choice(2,(n,2*p))
 print (X.shape)
-A=ssp.random(q,q,density=0.02)
+A=ssp.random(q,q)
 Omega=A.T@A+0.01*ssp.identity(q)
 Xnew=X.reshape((2*n,-1))
 #print(Y.shape)
@@ -29,9 +29,9 @@ Delta=ssp.eye(q, format="coo")
 Delta.setdiag(0.3, 0)
 
 #p*q
-Pi=ssp.rand(p,q,density=0.02)
+Pi=ssp.rand(p,q)
 #p*q
-Xi=ssp.rand(p,q,density=0.02)
+Xi=ssp.rand(p,q)
 #
 Lambda=ssp.kron(ssp.identity(m,format="coo"),Delta)+ssp.kron(np.ones((m,m)),Omega)
 Theta=ssp.vstack([ssp.kron(ssp.identity(m,format="coo"),Pi),ssp.kron(np.ones((1,m)),Xi)])
@@ -49,38 +49,53 @@ noiseY = np.random.multivariate_normal(np.zeros(m*q), Sigma.todense(), size=n)
 #n * mq 4*10
 Y = meanY + noiseY
 Ynew=Y.reshape((2*n,-1))
-#print(Y.shape)
 Ysum= Ynew[::2,:]+Ynew[1::2,:]
-for i in range(n):                                                                                                          
-	indices = np.random.choice(np.arange(q), replace=False, size=int(q * nan_percent_col))                                  
-	Y[i,indices]=float("nan")                                                                                               
-	Y[i,indices+q]=float("nan")
-# 8*5
-##print(Y)
-#print(Ysum)
-lambdaV=0.45
-lambdaGamma = 1.1
-lambdaF = 0.75
-lambdaPsi=0.6
+#print(Y.shape)
+
+for i in range(q):
+        l = np.random.choice(np.arange(n), replace=False, size=int(n*nan_percent_col))
+        Y[l,i]=float('nan')
+        Y[l,i+q]=float('nan')
+
+'''
+for i in l:
+        Y[:,i]=float('nan')
+        Y[:,i+q]=float('nan')
+
+for i in l:                                                                                                         
+        indices = np.random.choice(np.arange(q), replace=False, size=int(q * nan_percent_col))
+        Y[i,indices]=float("nan")                                                                                               
+        Y[i,indices+q]=float("nan")
+'''
+lambdaV=1.0
+lambdaGamma = 1.0
+lambdaF = 0.4
+lambdaPsi=0.45
 (V, F, Gamma, Psi, stats_sum) = q_mega_scggm(Y, X, Ysum, lambdaV, lambdaF, lambdaGamma, lambdaPsi)
 
+re_F=Xi+0.5*Pi
 re_Delta=m*Gamma
 re_Omega=V-Gamma
 re_Xi=F-Psi
 re_Pi=m*Psi
-#plt.figure()
-#plt.spy(V)
-#plt.title("V")
-#plt.savefig("./results/V.png")
 TP_x=0
 FP_x=0
 FN_x=0
-
+TP_f=0
+FP_f=0
+FN_f=0
+TP_o=0
+FP_o=0
+FN_o=0
 TP_p=0
 FP_p=0
 FN_p=0
 Pi_temp=Pi.tocsr()
 Xi_temp=Xi.tocsr()
+F_temp=re_F.tocsr()
+re_F_temp=F.tocsr()
+re_Omega_temp=re_Omega.tocsr()
+Omega_temp=Omega.tocsr()
 re_Pi_temp=re_Pi.tocsr()
 re_Xi_temp=re_Xi.tocsr()
 Pi_in_Xi=0
@@ -99,6 +114,20 @@ for i in range(p):
 				print(Pi_temp[i,j])
 				print(re_Pi_temp[i,j])
 				raise Exception("wrong Pi")
+		if not (math.isclose(F_temp[i,j],0.0) and math.isclose(re_F_temp[i,j],0.0)):
+
+			#if math.isclose(F_temp[i,j],re_F_temp[i,j]):
+			if (not math.isclose(F_temp[i,j],0.0)) and (not math.isclose(re_F_temp[i,j],0.0)):
+				TP_f+=1;
+			elif math.isclose(F_temp[i,j],0.0) and (not math.isclose(re_F_temp[i,j],0.0)):
+				FP_f+=1;
+			elif (not math.isclose(F_temp[i,j],0.0)) and math.isclose(re_F_temp[i,j],0.0):
+				FN_f+=1;
+			else:
+				print(F_temp[i,j])
+				print(re_F_temp[i,j])
+				raise Exception("wrong F")
+
 
 		if not (math.isclose(Xi_temp[i,j],0.0) and math.isclose(re_Xi_temp[i,j],0.0)):
 			if (not math.isclose(Xi_temp[i,j],0.0)) and (not math.isclose(re_Xi_temp[i,j],0.0)):
@@ -115,19 +144,118 @@ for i in range(p):
 			if not math.isclose(Pi_temp[i,j],0.0):
 				Pi_in_Xi+=1;
 
+for i in range(q):
+	for j in range(q):
+		if not (math.isclose(Omega_temp[i,j],0.0) and math.isclose(re_Omega_temp[i,j],0.0)):
 
+			#if math.isclose(Omega_temp[i,j],re_Omega_temp[i,j]):
+			if (not math.isclose(Omega_temp[i,j],0.0)) and (not math.isclose(re_Omega_temp[i,j],0.0)):
+				TP_o+=1;
+			elif math.isclose(Omega_temp[i,j],0.0) and (not math.isclose(re_Omega_temp[i,j],0.0)):
+				FP_o+=1;
+			elif (not math.isclose(Omega_temp[i,j],0.0)) and math.isclose(re_Omega_temp[i,j],0.0):
+				FN_o+=1;
+			else:
+				print(Omega_temp[i,j])
+				print(re_Omega_temp[i,j])
+				raise Exception("wrong Omega")
+
+	
 precision_p=TP_p/(TP_p+FP_p)
 recall_p=TP_p/(TP_p+FN_p)
 
 precision_x=TP_x/(TP_x+FP_x)
 recall_x=TP_x/(TP_x+FN_x)
 
-with open('./results/precision_recall.txt', 'w') as output_file:
+precision_f=TP_f/(TP_f+FP_f)
+recall_f=TP_f/(TP_f+FN_f)
+
+precision_o=TP_o/(TP_o+FP_o)
+recall_o=TP_o/(TP_o+FN_o)
+
+with open('./results-4/precision_recall.txt', 'w') as output_file:
     output_file.write('Pi precision: %f\n' % (precision_p))
     output_file.write('Pi recall: %f\n' % (recall_p))
     output_file.write('Xi precision: %f\n' % (precision_x))
     output_file.write('Xi recall: %f\n' % (recall_x))
+    output_file.write('F precision: %f\n' % (precision_f))
+    output_file.write('F recall: %f\n' % (recall_f))
+    output_file.write('Omega precision: %f\n' % (precision_o))
+    output_file.write('Omega recall: %f\n' % (recall_o))
+    
+    
     output_file.write('%d true Pi appear in estimating Xi\n' % (Pi_in_Xi))
+with open('pi.txt','a+') as f:
+    f.seek(0)
+    c=f.readlines()
+    l=np.array(c).astype(np.float)
+    x=np.append(l[::2],recall_p).tolist()
+    y=np.append(l[1::2],precision_p).tolist()
+    plt.figure()
+    x_y=list(zip(x,y))
+    x_y=sorted(x_y, key = lambda x: x[0])
+    x=[i for i,j in x_y]
+    y=[j for i,j in x_y]
+    plt.plot(x,y,scalex=False, scaley=False)
+    plt.xlabel('Pi Recall')
+    plt.ylabel('Pi Precision')
+    plt.savefig('./results-4/pr_p.png')
+    f.seek(2)
+    f.write('%f\n' % (recall_p))
+    f.write('%f\n' % (precision_p))
+with open('xi.txt','a+') as f:
+    f.seek(0)    
+    l=np.array(f.readlines()).astype(np.float)
+    x=np.append(l[::2],recall_x).tolist()
+    y=np.append(l[1::2],precision_x).tolist()
+    plt.figure()
+    x_y=list(zip(x,y))
+    x_y=sorted(x_y, key = lambda x: x[0])
+    x=[i for i,j in x_y]
+    y=[j for i,j in x_y]
+    plt.plot(x,y,scalex=False, scaley=False)
+    plt.xlabel('Xi Recall')
+    plt.ylabel('Xi Precision')
+    plt.savefig('./results-4/pr_x.png')
+    f.seek(2)
+    f.write('%f\n' % (recall_x))
+    f.write('%f\n' % (precision_x))
+
+with open('f.txt','a+') as f:
+    f.seek(0)
+    l=np.array(f.readlines()).astype(np.float)
+    x=np.append(l[::2],recall_f).tolist()
+    y=np.append(l[1::2],precision_f).tolist()   
+    x_y=list(zip(x,y))
+    x_y=sorted(x_y, key = lambda x: x[0])
+    x=[i for i,j in x_y]
+    y=[j for i,j in x_y]
+    plt.figure()
+    plt.plot(x,y,scalex=False, scaley=False)
+    plt.xlabel('F Recall')
+    plt.ylabel('F Precision')
+    plt.savefig('./results-4/pr_f.png')
+    f.seek(2)
+    f.write('%f\n' % (recall_f))
+    f.write('%f\n' % (precision_f))
+with open('o.txt','a+') as f:
+    f.seek(0)
+    l=np.array(f.readlines()).astype(np.float)
+    x=np.append(l[::2],recall_o).tolist()
+    y=np.append(l[1::2],precision_o).tolist()
+    x_y=list(zip(x,y))
+    x_y=sorted(x_y, key = lambda x: x[0])
+    x=[i for i,j in x_y]
+    y=[j for i,j in x_y]
+   
+    plt.figure()
+    plt.plot(x,y,scalex=False, scaley=False)
+    plt.xlabel('Omega Recall')
+    plt.ylabel('Oemga Precision')
+    plt.savefig('./results-4/pr_o.png')
+    f.seek(2)
+    f.write('%f\n' % (recall_o))
+    f.write('%f\n' % (precision_o))
 
 plt.figure()
 #plt.spy(Delta)
@@ -136,56 +264,65 @@ temp=plt.imshow(Delta.todense(), cmap='Blues',interpolation="nearest")
 plt.colorbar(temp)
 
 plt.title("Delta")
-plt.savefig("./results/Delta.png")
+plt.savefig("./results-4/Delta.png")
 plt.figure()
 #plt.spy(re_Delta)
 temp=plt.imshow(np.abs(re_Delta.todense()), cmap='Blues',interpolation="nearest")
 # pass this heatmap object into plt.colorbar method.
 plt.colorbar(temp)
 plt.title("re_Delta")
-plt.savefig("./results/re_Delta.png")
+plt.savefig("./results-4/re_Delta.png")
 plt.figure()
 Omega.setdiag(0.0,0)
 temp=plt.imshow(np.abs(Omega.todense()), cmap='Blues',interpolation="nearest")
 # pass this heatmap object into plt.colorbar method.
 plt.colorbar(temp)
 plt.title("Omega")
-plt.savefig("./results/Omega.png")
+plt.savefig("./results-4/Omega.png")
 plt.figure()
 re_Omega.setdiag(0.0,0)
 temp=plt.imshow(np.abs(re_Omega.todense()), cmap='Blues',interpolation="nearest")
 # pass this heatmap object into plt.colorbar method.
 plt.colorbar(temp)
-plt.savefig("./results/re_Omega.png")
+plt.savefig("./results-4/re_Omega.png")
 plt.figure()
 temp=plt.imshow(np.abs(Xi.todense()), cmap='Blues',interpolation="nearest")
 # pass this heatmap object into plt.colorbar method.
 plt.colorbar(temp)
 plt.title("Xi")
-plt.savefig("./results/Xi.png")
+plt.savefig("./results-4/Xi.png")
 plt.figure()
 etmp=plt.imshow(np.abs(re_Xi.todense()), cmap='Blues',interpolation="nearest")
 # pass this heatmap object into plt.colorbar method.
 plt.colorbar(temp)
 plt.title("re_Xi")
-plt.savefig("./results/re_Xi.png")
+plt.savefig("./results-4/re_Xi.png")
 plt.figure()
 temp=plt.imshow(np.abs(Pi.todense()), cmap='Blues',interpolation="nearest")
 # pass this heatmap object into plt.colorbar method.
 plt.colorbar(temp)
 plt.title("Pi")
-plt.savefig("./results/Pi.png")
+plt.savefig("./results-4/Pi.png")
 plt.figure()
 temp=plt.imshow(np.abs(re_Pi.todense()), cmap='Blues',interpolation="nearest")
 # pass this heatmap object into plt.colorbar method.
 plt.colorbar(temp)
 plt.title("re_Pi")
 
-plt.savefig("./results/re_Pi.png")
+plt.savefig("./results-4/re_Pi.png")
+
 plt.figure()
 temp=plt.imshow(np.abs(F.todense()), cmap='Blues',interpolation="nearest")
 # pass this heatmap object into plt.colorbar method.
 plt.colorbar(temp)
 plt.title("F")
 
-plt.savefig("./results/F.png")
+plt.savefig("./results-4/F.png")
+plt.figure()
+temp=plt.imshow(np.abs(re_F.todense()), cmap='Blues',interpolation="nearest")
+# pass this heatmap object into plt.colorbar method.
+plt.colorbar(temp)
+plt.title("re_F")
+
+plt.savefig("./results-4/re_F.png")
+
